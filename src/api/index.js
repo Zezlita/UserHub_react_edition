7763c346b83046737ef64dd142e0c153457e3ef5
CeciliaLam <cecilia.hoszelam@gmail.com>
2020-10-31 12:58:54 -0700

    test pushing userHub

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const BASE = 'https://jsonplace-univclone.herokuapp.com'
+
+export async function getUsers(){
+    try{
+        const {data} = await axios.get(`${BASE}/users`);
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function getPostsByUser(userId)
