b0986ec8fbba8e565dcfa646e2f57827f1f79f82
CeciliaLam <cecilia.hoszelam@gmail.com>
2020-11-01 00:31:44 -0700

    completing userTodos

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,38 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './header.css';
 
-const Header = ({userList}) => {
+const Header = ({
+    currentUser,
+    setCurrentUser,
+    userList
+    }) => {
+    const [selectedUser, setSelectedUser] = useState();
+    // First we create a piece of local state for our form. Then, we create an effect that depends on the value of userList. This effect will run when the component is created, and will re-run when our App finishes its own effect.
+
+    //That second time will set the default selectedUser to the first one in our dropdown.
+    useEffect (() => {
+        setSelectedUser(userList[0]);
+    }, [userList]);
+
     const handleSubmit = (event) => {
         event.preventDefault();
     }
     const handleSelectChange = (event) => {
-        
+        const id = event.target.value;
+        const user = userlist.find(user => user.id == id);
+        setSelectedUser(user);
     }
 
-    const handleUserLogin = (event) => {
+    //Lastly, depending on if a user is logging in or logging out we update the value of currentUser by calling setCurrentUser either on selectedUser or null.
 
+    //If you use the log in and log out features you should see that we can, in fact, update the value of currentUser and that the form updates accordingly.
+    const handleUserLogin = (event) => {
+        setCurrentUser(selectedUser);
     }
 
     const handleUserLogout = (event) => {
-
+        setSelectedUser(userList[0]);
+        setCurrentUser(null);
     }
 
     return (
@@ -22,8 +40,12 @@ const Header = ({userList}) => {
             <h1>Welcome to UserHub</h1>
             <form
                 className = "user-select"
-                onSubmit={handleSubmit}>
-                <select onChange={ handleSelectChange } > {
+                onSubmit={handleSubmit}> 
+                {
+                    currentUser 
+                    ? <button onClick={ handleUserLogout }>LOG OUT</button>
+                    : <>
+                    <select onChange={ handleSelectChange } > {
                     userList.map(user => (
                         <option key={user.id} value={user.id}>
                             {user.username}
@@ -31,7 +53,8 @@ const Header = ({userList}) => {
                     ))
             }</select>
             <button onClick={handleUserLogin} >LOG IN</button>
-            <button onClick={handleUserLogout} >LOG OUT</button>
+            </>
+            }
             </form>
         </header>
     );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import {getUsers} from './api';
 import ReactDOM from 'react-dom';
 
 import {
-  Header
+  Header,
+  UserPosts,
+  UserTodos
 } from './components';
+
+import {
+  getUsers,
+  getPostsByUser,
+  getTodosByUser
+} from './api';
 
 //THE DATA FLOW -- Inside our main App component, we initialize a piece of state called userList, and set it equal to an empty array. The App component calls the Header component, passing the userList along.
 const App = () => {
   const [userList, setUserList] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [userPosts, setUserPosts] = useState([]);
+  const [userTodos, setUserTodos] = useState([]);
 
   useEffect(() => {
     getUsers()
@@ -19,11 +29,50 @@ const App = () => {
         console.error
       });
   }, []);
+
+  //Whenever the value of currentUser changes (via login/logout) we will run this effect. We will either set userPosts to an empty array (if there is no currentUser), or we will make a call to the API and set the userPosts to the return value of that if there is a currentUser.
+
+  //Lastly we use conditional rendering to show the PostList component only if there is a currentUser set.
+
+  useEffect(() => {
+    if(!currentUser) {
+      setUserPosts([]);
+      setUserTodos([]);
+      return;
+    }
+    getPostsByUser(currentUser.id)
+      .then(posts => {
+        setUserPosts(posts);
+      })
+      .catch(error => {
+        console.error
+      });
+    getTodosByUser(currentUser.id)
+      .then(todos => {
+        setUserTodos(todos);
+      })
+      .catch(error => {
+        console.error
+      })
+  }, [currentUser]);
+
   //The array of dependencies should be a list of state or props you care about such that, if any of them change, we would rerun the effect function. In this case we gave an empty array which tells useEffect never to re-run the effect function (since it doesn't depend on any prop or state to be run).
   
   return (
     <div id="App">
-      <Header userList={userList} />
+      <Header 
+      userList={userList} 
+      currentUser={currentUser}
+      setCurrentUser={setCurrentUser}
+      />
+      {
+      currentUser
+      ? <>
+      <UserPosts userPosts={userPosts} currentUser={currentUser} />
+      <UserTodos userTodos={userTodos} currentUser={currentUser} />
+      </>
+      : null
+        }
     </div>
   );
 }
