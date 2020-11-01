6b275d62947691160f67ba7f159d1bfe5ae4d0f4
CeciliaLam <cecilia.hoszelam@gmail.com>
2020-10-31 21:55:08 -0700

    getting login logout process done

---
+++ src/components/header.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import './header.css';
+
+const Header = ({userList}) => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+    const handleSelectChange = (event) => {
+        
+    }
+
+    const handleUserLogin = (event) => {
+
+    }
+
+    const handleUserLogout = (event) => {
+
+    }
+
+    return (
+        <header>
+            <h1>Welcome to UserHub</h1>
+            <form
+                className = "user-select"
+                onSubmit={handleSubmit}>
+                <select onChange={ handleSelectChange } > {
+                    userList.map(user => (
+                        <option key={user.id} value={user.id}>
+                            {user.username}
+                        </option>
+                    ))
+            }</select>
+            <button onClick={handleUserLogin} >LOG IN</button>
+            <button onClick={handleUserLogout} >LOG OUT</button>
+            </form>
+        </header>
+    );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import {getUsers} from './api';
 import ReactDOM from 'react-dom';
 
+import {
+  Header
+} from './components';
+
+//THE DATA FLOW -- Inside our main App component, we initialize a piece of state called userList, and set it equal to an empty array. The App component calls the Header component, passing the userList along.
 const App = () => {
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    getUsers()
+      .then(users => {
+        setUserList(users)
+      })
+      .catch(error => {
+        console.error
+      });
+  }, []);
+  //The array of dependencies should be a list of state or props you care about such that, if any of them change, we would rerun the effect function. In this case we gave an empty array which tells useEffect never to re-run the effect function (since it doesn't depend on any prop or state to be run).
+  
   return (
     <div id="App">
-      <h1>Hello, World</h1>
+      <Header userList={userList} />
     </div>
   );
 }
